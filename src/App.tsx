9df9cc590c82a90ba9dd4f27b6c088f4bf67d13e
Subodh Kshirsagar Kamble <118099441+subodh245@users.noreply.github.com>
2024-01-15 04:34:03 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import "./App.css";
+import InputFeild from "./components/InputFeild";
+import { Todo } from "./components/model";
+import TodoList from "./components/TodoList";
+
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+
+  console.log(todo);
+
+  return (
+    <div className="App">
+      <span className="heading">Taskify</span>
+      <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
+    </div>
+  );
+};
+
+export default App;
