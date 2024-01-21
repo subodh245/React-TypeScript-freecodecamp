899b8cadd16242529ffd1299096f04e09d5801c1
Subodh Kshirsagar Kamble <118099441+subodh245@users.noreply.github.com>
2024-01-22 02:53:20 +0530

    Add files via upload

---
+++ src/src/components/TodoList.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import "./style.css";
+import { Todo } from "./model";
+import SingleTodo from "./SingleTodo";
+import { Droppable } from "react-beautiful-dnd";
+
+interface Props {
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+  completedTodos: Todo[];
+  setCompletedTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const TodoList: React.FC<Props> = ({
+  todos,
+  setTodos,
+  completedTodos,
+  setCompletedTodos,
+  
+}: Props) => {
+  return (
+    <div className="container">
+      <Droppable droppableId="TodoList">
+        {(provided) => (
+          <div
+            className="todos"
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <span className="todos_heading">Active Tasks</span>
+            {todos.map((todo, index) => (
+              <SingleTodo
+                index={index}
+                todo={todo}
+                todos={todos}
+                key={todo.id}
+                setTodos={setTodos}
+              />
+            ))}
+          </div>
+        )}
+      </Droppable>
+
+      <Droppable droppableId="TodoRemove">
+        {(provided) => (
+          <div
+            className="todos remove"
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <span className="todos_heading">Completed Tasks</span>
+            {completedTodos.map((todo, index) => (
+              <SingleTodo
+                index={index}
+                todo={todo}
+                todos={completedTodos}
+                key={todo.id}
+                setTodos={setCompletedTodos}
+              />
+            ))}
+          </div>
+        )}
+      </Droppable>
+    </div>
+  );
+};
+
+export default TodoList;
---
+++ src/src/components/reducer.tsx
@@ -0,0 +1,27 @@
+// // type Action =
+// // |{type:"add"l payload: string}
+// // |{type:"remove"; payload:number}
+// // |{type: "done"; payload:number};
+
+// // const TodoReducer = (state:Todo[],action:Actions) => {
+// //     switch (action.type) {
+// //         case "add":
+// //             return[
+// //                 ...state,
+// //                 {id:Date.now(), todo:action.payload,isDone:false},
+
+// //             ];
+// //         case"remove":
+// //         return state.filter((todo) => todo.id !==action.payload);
+// //         case"done":
+// //         return state.map((todo)
+// //         todo.id !== action.payload ? {...todo, isDone:});
+// //     }
+// // }
+
+// import { useReducer } from "react";
+// const ReducerExample = () => {
+//     const [state, dispatcjj]
+// }
+
+export {};
